381ca1f7b6bf2cd17fb57eee62e9239fc3b24a81
Pinto <pinto702rodri@gmail.com>
2025-11-10 23:06:18 -0500

    :construction: Reviewing object-oriented programming in Node.js

---
+++ Dia3/sih.js
@@ -0,0 +1,25 @@
+
+/*En esta parte se va a importar todas las clases 
+y constantes que se van a usar requiere() carga el modulo sih.js
+este modulo esporta objetos con muchas propiedades 
+(Es como queremos recibir el formato)
+*/
+
+const {
+    nombre, direccion, paciente,
+    rangoDoctor, doctor, enfermera,
+    personalTecnico, tipoTecnico,
+    departamento, pabellon, hospital,
+} = require('./sih.js');
+
+const titulo = (s) => console.log (`\n=== ${s} ===`);
+
+/*Aca vamos a crear hospital, departamentos y pabellones */
+
+const hospital1 = new hospital({nombre: 'hospital central'});
+const depCardio = hospital1.agregarDepartamento(new departamento({nombre: 'cardioloía'}));
+const depTI = hospital1.agregarDepartamento(new departamento( {nombre: 'tecnologia'}));
+
+
+const pabCardio = hospital1.agregarPabellon(new pabellon ({nombre:'pabellon cardio', capacidad: '2', especialidad: 'CARDIOLOGIA'}));
+const pabGeneral = hospital1.agregarPabellon(new pabellon({nombre: 'pabellon general', capacidad: '4'}));
