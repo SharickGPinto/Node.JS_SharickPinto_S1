7480d0a94bfd8aa7a3901d518c19d7a0df6addc1
Pinto <pinto702rodri@gmail.com>
2025-11-11 09:18:55 -0500

    :construction: correcting classwork

---
+++ Dia3/Tarea1/models/Doctor.js
@@ -0,0 +1,6 @@
+class doctor{
+    constructor(tituloTecnico, nombre ){
+        this.nombre = nombre, this.tituloTecnico = tituloTecnico
+    }
+}
+module.exports = doctor; 
---
+++ Dia3/Tarea1/models/Paciente.js
@@ -0,0 +1,8 @@
+class paciente {
+    constructor(nombre, direccion){
+  this.nombre = nombre, this.direccion = direccion
+    }
+    
+}
+
+module.exports = paciente;
