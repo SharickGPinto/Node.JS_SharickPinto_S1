1c665253a13d856accab4810929ff0d41af29d80
Pinto <pinto702rodri@gmail.com>
2025-11-13 21:55:16 -0500

    :construction: correcting classwork

--- Dia5/workshop/SRP/domain/priceCalculator.js
+++
@@ -1,7 +0,0 @@
-export class priceCalculator{
-    compute(basePrice, coupon){
-        if (!coupon) return basePrice;
-        if (coupon.type === "perfect") return basePrice - basePrice * (coupon.value / 100);
-        if (coupon.type === "fixed")
-    }
-}
